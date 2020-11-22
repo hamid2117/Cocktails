3cc832add5dff47fe2aeac787c87055ee81e8a48
hamid2117 <saim008865@gmail.com>
2020-11-22 08:32:48 +0500

    first commits

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor
+        doloribus ullam velit necessitatibus soluta vero sunt consequatur quo
+        eius eveniet minima at sit qui odit alias nam iure possimus, nemo dicta!
+        Consectetur ipsa molestias commodi, non perspiciatis animi
+        necessitatibus ipsum?
+      </p>
+    </section>
+  )
+}
+
+export default About
